73689222224a9b810167856b42c54871af7c1bb8
Hadi Mohammadi <rahnemon2011@gmail.com>
2018-09-06 11:24:11 +0200

    #3 Add Spring Batch artifact to gradle

--- src/main/java/io/knowledgeassets/myskills/server/report/batch/BatchConfiguration.java
+++ src/main/java/io/knowledgeassets/myskills/server/report/batch/BatchConfiguration.java
@@ -1,5 +1,12 @@
-//package io.knowledgeassets.myskills.server.userskillpriorityreport.batch;
+//package io.knowledgeassets.myskills.server.report.batch;
 //
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.core.launch.support.RunIdIncrementer;
+//import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
