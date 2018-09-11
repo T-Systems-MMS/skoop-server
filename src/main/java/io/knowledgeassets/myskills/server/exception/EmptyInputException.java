4aa823ea40364b1f8cd68bf8f319f6e36ea038c1
Hadi Mohammadi <rahnemon2011@gmail.com>
2018-09-11 11:34:51 +0200

    #3 Change name of InvalidInputException to EmptyInputException

--- src/main/java/io/knowledgeassets/myskills/server/exception/EmptyInputException.java
+++ src/main/java/io/knowledgeassets/myskills/server/exception/EmptyInputException.java
@@ -7,10 +7,10 @@ import lombok.Builder;
  *
  * @author hadi
  */
-public class InvalidInputException extends BusinessException {
+public class EmptyInputException extends BusinessException {
 
 	@Builder
-	public InvalidInputException(String message, Long code) {
+	public EmptyInputException(String message, Long code) {
 		super(message, code);
 	}
 }
