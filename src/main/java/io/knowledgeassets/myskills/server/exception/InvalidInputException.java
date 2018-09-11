e04900628a48fd2f606b970296a7f6852c8cfdb1
Hadi Mohammadi <rahnemon2011@gmail.com>
2018-09-11 10:03:57 +0200

    #3 Add @Builder to InvalidInputException constructor

--- src/main/java/io/knowledgeassets/myskills/server/exception/InvalidInputException.java
+++ src/main/java/io/knowledgeassets/myskills/server/exception/InvalidInputException.java
@@ -1,5 +1,7 @@
 package io.knowledgeassets.myskills.server.exception;
 
+import lombok.Builder;
+
 /**
  * When a <b>field data</b> is empty, but we expect to be full, we throw this exception.
  *
@@ -7,6 +9,7 @@ package io.knowledgeassets.myskills.server.exception;
  */
 public class InvalidInputException extends BusinessException {
 
+	@Builder
 	public InvalidInputException(String message, Long code) {
 		super(message, code);
 	}
