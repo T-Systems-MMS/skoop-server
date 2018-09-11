055fffd9a7f72ac2f9d737583ef3267910c7980c
Hadi Mohammadi <rahnemon2011@gmail.com>
2018-09-11 11:34:29 +0200

    #3 Add below Items to Model enum 	UserSkillPriorityReport("user skill priority report"), 	UserSkillPriorityAggregationReport("user skill priority aggregation report"), 	UserSkillReport("user skill report"),

--- src/main/java/io/knowledgeassets/myskills/server/exception/enums/Model.java
+++ src/main/java/io/knowledgeassets/myskills/server/exception/enums/Model.java
@@ -1,5 +1,7 @@
 package io.knowledgeassets.myskills.server.exception.enums;
 
+import io.knowledgeassets.myskills.server.report.userskillpriorityaggregationreport.UserSkillPriorityAggregationReport;
+
 /**
  * Each model points an Entity. We usually use it in exception messages.
  *
@@ -9,7 +11,12 @@ public enum Model {
 
 	USER("user"),
 	SKILL("skill"),
-	USERSKILL("user skill");
+	USERSKILL("user skill"),
+
+	UserSkillPriorityReport("user skill priority report"),
+	UserSkillPriorityAggregationReport("user skill priority aggregation report"),
+	UserSkillReport("user skill report"),
+	;
 
 	public final String value;
 
