963f7a7f62b3b9318f553d72f182f89c2a2da0d1
Hadi Mohammadi <rahnemon2011@gmail.com>
2018-09-11 08:41:50 +0200

    #3 Use Spring AOP to check BindingResult in our controller If There is an exception in BindingResult, we handle it, and create appropriate exception in BindingResultAspect.

---
+++ src/main/java/io/knowledgeassets/myskills/server/aspect/CheckBindingResult.java
@@ -0,0 +1,17 @@
+package io.knowledgeassets.myskills.server.aspect;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A marker annotation to be used by BindingResultAspect to make the aspect limited.
+ *
+ * @author hadi
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CheckBindingResult {
+
+}
