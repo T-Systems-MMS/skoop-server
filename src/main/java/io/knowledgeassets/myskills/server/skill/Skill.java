76db13c89caf5caddd96909272f662750f18d819
Hadi Mohammadi <rahnemon2011@gmail.com>
2018-09-13 10:00:25 +0200

    #7 Add @Index(unique = true) to Skill Node

--- src/main/java/io/knowledgeassets/myskills/server/skill/Skill.java
+++ src/main/java/io/knowledgeassets/myskills/server/skill/Skill.java
@@ -2,15 +2,16 @@ package io.knowledgeassets.myskills.server.skill;
 
 import io.knowledgeassets.myskills.server.userskill.UserSkill;
 import lombok.*;
-import org.neo4j.ogm.annotation.Id;
-import org.neo4j.ogm.annotation.NodeEntity;
-import org.neo4j.ogm.annotation.Property;
-import org.neo4j.ogm.annotation.Relationship;
+import org.neo4j.ogm.annotation.*;
+import org.neo4j.ogm.session.event.Event;
+import org.neo4j.ogm.session.event.EventListener;
+import org.neo4j.ogm.session.event.EventListenerAdapter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @Builder
@@ -26,10 +27,13 @@ public class Skill {
 	@NotBlank
 	@Size(max = 64)
 	@Property(name = "name")
+	@Index(unique = true)
 	private String name;
+
 	@Property(name = "description")
 	private String description;
 	@EqualsAndHashCode.Exclude
 	@Relationship(type = "RELATED_TO", direction = Relationship.UNDIRECTED)
 	private List<UserSkill> userSkills;
+
 }
