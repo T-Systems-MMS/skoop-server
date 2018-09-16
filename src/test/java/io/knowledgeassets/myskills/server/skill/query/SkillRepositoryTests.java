d259ebf9b0f369c980fe5a64be5cc6865637a70b
Georg Wittberger <georg.wittberger@gmail.com>
2018-09-16 11:40:29 +0200

    #6 Add skill repository tests

--- src/test/java/io/knowledgeassets/myskills/server/skill/query/SkillRepositoryTests.java
+++ src/test/java/io/knowledgeassets/myskills/server/skill/query/SkillRepositoryTests.java
@@ -2,6 +2,7 @@ package io.knowledgeassets.myskills.server.skill.query;
 
 import io.knowledgeassets.myskills.server.skill.Skill;
 import io.knowledgeassets.myskills.server.skill.SkillRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,8 +21,8 @@ class SkillRepositoryTests {
 	private SkillRepository skillRepository;
 
 	@Test
-	@DisplayName("Provides the existing skill queried by its name")
-	void providesSkillByName() {
+	@DisplayName("Provides the existing skill queried by its exact name")
+	void providesSkillByExactName() {
 		// Given
 		skillRepository.save(Skill.builder().id("123").name("Angular").description("JavaScript Framework").build());
 		// When
@@ -31,5 +32,44 @@ class SkillRepositoryTests {
 		assertThat(skill.get().getId()).isEqualTo("123");
 		assertThat(skill.get().getName()).isEqualTo("Angular");
 		assertThat(skill.get().getDescription()).isEqualTo("JavaScript Framework");
+	}
+
+	@Test
+	@DisplayName("Provides the existing skill queried by its name ignoring case")
+	@Disabled("SDN 5.0.x does not support 'IgnoreCase' in finder methods yet. " +
+			"This will be implemented with SDN 5.1, " +
+			"see https://docs.spring.io/spring-data/neo4j/docs/5.1.0.RC2/reference/html/#_query_methods")
+	void providesSkillByNameIgnoringCase() {
+		// Given
+		skillRepository.save(Skill.builder().id("123").name("Angular").description("JavaScript Framework").build());
+		// When
+		Optional<Skill> skill = skillRepository.findByNameIgnoreCase("anGulaR");
+		// Then
+		assertThat(skill).isNotEmpty();
+		assertThat(skill.get().getId()).isEqualTo("123");
+		assertThat(skill.get().getName()).isEqualTo("Angular");
+		assertThat(skill.get().getDescription()).isEqualTo("JavaScript Framework");
+	}
+
+	@Test
+	@DisplayName("Indicates that the skill with a given name exists ignoring case")
+	void indicatesExistingSkillByNameIgnoringCase() {
+		// Given
+		skillRepository.save(Skill.builder().id("123").name("Angular").description("JavaScript Framework").build());
+		// When
+		Boolean skillExists = skillRepository.isSkillExistByNameIgnoreCase("anGulaR");
+		// Then
+		assertThat(skillExists).isTrue();
+	}
+
+	@Test
+	@DisplayName("Indicates that the skill with a given name does not exist")
+	void indicatesNonExistingSkillByName() {
+		// Given
+		skillRepository.save(Skill.builder().id("123").name("Angular").description("JavaScript Framework").build());
+		// When
+		Boolean skillExists = skillRepository.isSkillExistByNameIgnoreCase("spring");
+		// Then
+		assertThat(skillExists).isFalse();
 	}
 }
