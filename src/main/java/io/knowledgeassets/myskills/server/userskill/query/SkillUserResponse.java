23605dc5d55839213ae5fba16721c0d5efcdcb17
Georg Wittberger <georg.wittberger@gmail.com>
2018-10-06 16:26:53 +0200

    Fix missing coach flag in skill users API

--- src/main/java/io/knowledgeassets/myskills/server/userskill/query/SkillUserResponse.java
+++ src/main/java/io/knowledgeassets/myskills/server/userskill/query/SkillUserResponse.java
@@ -5,20 +5,24 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
-import org.neo4j.ogm.annotation.Property;
 
 @Data
 @Builder
-@ApiModel(value = "SkillUserResponse",
+@ApiModel(
+		value = "SkillUserResponse",
 		description = "Relationship to a user from a skill perspective including the user details, the current skill " +
-				"level of the user, the desired skill level of the user and the priority to reach the desired level.")
+				"level of the user, the desired skill level of the user and the priority to reach the desired level."
+)
 public class SkillUserResponse {
 	@ApiModelProperty("Details of the related user")
 	private UserResponse user;
+
 	@ApiModelProperty("Current skill level of the user (range: 0-4 where 0 = no knowledge at all, 4 = expert)")
 	private Integer currentLevel;
+
 	@ApiModelProperty("Desired skill level of the user (range: 0-4 where 0 = no knowledge at all, 4 = expert)")
 	private Integer desiredLevel;
+
 	@ApiModelProperty("Priority to reach the desired level (range: 0-4 where 0 = no action needed, 4 = urgent)")
 	private Integer priority;
 }
