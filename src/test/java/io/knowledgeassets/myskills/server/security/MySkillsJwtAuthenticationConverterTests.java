8f803e64c601bdf6ef56c333bc8665514f5d3fb7
Dmitry Lebedko <dmitry.lebedko@t-systems.com>
2019-01-11 16:40:00 +0100

    Roles as part of JWT are provided in json file.

--- src/test/java/io/knowledgeassets/myskills/server/security/MySkillsJwtAuthenticationConverterTests.java
+++ src/test/java/io/knowledgeassets/myskills/server/security/MySkillsJwtAuthenticationConverterTests.java
@@ -1,14 +1,15 @@
 package io.knowledgeassets.myskills.server.security;
 
-
-import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
+import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Collection;
@@ -28,30 +29,19 @@ class MySkillsJwtAuthenticationConverterTests {
 
 	@Test
 	@DisplayName("Roles are extracted as granted authorities both from realm and resource access.")
-	void rolesAreExtractedAsGrantedAuthoritiesBothFromRealmAndResourceAccess() {
-		JSONObject claims = new JSONObject();
-		claims.put("azp", "myskills");
-		JSONObject resourceAccess = new JSONObject();
-		JSONObject mySkills = new JSONObject();
-		JSONArray roles = new JSONArray();
-		roles.add("ADMIN");
-		roles.add("USER");
-		mySkills.put("roles", roles);
-		resourceAccess.put("myskills", mySkills);
-		claims.put("resource_access", resourceAccess);
-		JSONObject realmAccess = new JSONObject();
-		JSONArray realmRoles = new JSONArray();
-		realmRoles.add("REALM");
-		realmAccess.put("roles", realmRoles);
-		claims.put("realm_access", realmAccess);
-		Collection<GrantedAuthority> grantedAuthorities = mySkillsJwtAuthenticationConverter.extractAuthorities(new Jwt("token",
-				LocalDateTime.of(2019, 1, 1, 10, 0).toInstant(ZoneOffset.UTC),
-				LocalDateTime.of(2019, 1, 2, 10, 0).toInstant(ZoneOffset.UTC),
-				Collections.singletonMap("key", "value"),
-				claims));
-		assertThat(grantedAuthorities).hasSize(3);
-		assertThat(grantedAuthorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
-				.containsExactlyInAnyOrder("ROLE_ADMIN", "ROLE_USER", "ROLE_REALM");
+	void rolesAreExtractedAsGrantedAuthoritiesBothFromRealmAndResourceAccess() throws Exception {
+		final ClassPathResource body = new ClassPathResource("roles_as_part_of_jwt.json");
+		try (final InputStream is = body.getInputStream()) {
+			JSONObject claims = new JSONParser(JSONParser.MODE_PERMISSIVE).parse(is, JSONObject.class);
+			Collection<GrantedAuthority> grantedAuthorities = mySkillsJwtAuthenticationConverter.extractAuthorities(new Jwt("token",
+					LocalDateTime.of(2019, 1, 1, 10, 0).toInstant(ZoneOffset.UTC),
+					LocalDateTime.of(2019, 1, 2, 10, 0).toInstant(ZoneOffset.UTC),
+					Collections.singletonMap("key", "value"),
+					claims));
+			assertThat(grantedAuthorities).hasSize(3);
+			assertThat(grantedAuthorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+					.containsExactlyInAnyOrder("ROLE_ADMIN", "ROLE_USER", "ROLE_REALM");
+		}
 	}
 
 }
