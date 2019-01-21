b0717f8281d2de5e16f579749333712f85555c10
Svetlana Ivanova <svetlana.ivanova.1@ya.ru>
2019-01-21 17:46:26 +0300

    neo4j indexes creation on application startup (#25)
    * neo4j indexes creation on application startup
    * create indexes from a dump file
    * add more tests
    * consolidate IndexManager with IndexService
    * remove DumpNeo4jProperties
    * remove the auto-index config

---
+++ src/main/java/io/knowledgeassets/myskills/server/neo4j/IndexType.java
@@ -0,0 +1,140 @@
+package io.knowledgeassets.myskills.server.neo4j;
+
+import java.util.Arrays;
+
+public enum IndexType {
+
+	/**
+	 * Single property neo4j
+	 */
+	SINGLE_INDEX {
+		@Override
+		public String indexCommand(String owningType, String[] properties) {
+			if (properties.length != 1) {
+				throw new IllegalArgumentException(SINGLE_INDEX + " must have exactly one property, got " +
+						Arrays.toString(properties));
+			}
+			return "INDEX ON :`" + owningType + "`(`" + properties[0] + "`)";
+		}
+	},
+
+	/**
+	 * Composite neo4j
+	 */
+	COMPOSITE_INDEX {
+		@Override
+		public String indexCommand(String owningType, String[] properties) {
+			StringBuilder sb = new StringBuilder();
+			sb.append("INDEX ON :`")
+					.append(owningType)
+					.append("`(");
+			appendProperties(sb, properties);
+			sb.append(")");
+			return sb.toString();
+		}
+	},
+
+	/**
+	 * Unique constraint
+	 */
+	UNIQUE_CONSTRAINT {
+		@Override
+		public String indexCommand(String owningType, String[] properties) {
+			String name = owningType.toLowerCase();
+
+			if (properties.length != 1) {
+				throw new IllegalArgumentException(UNIQUE_CONSTRAINT + " must have exactly one property, got " +
+						Arrays.toString(properties));
+			}
+			return "CONSTRAINT ON (`" + name + "`:`" + owningType + "`) ASSERT `" + name + "`.`" + properties[0]
+					+ "` IS UNIQUE";
+		}
+	},
+
+	/**
+	 * Node key constraint
+	 */
+	NODE_KEY_CONSTRAINT {
+		@Override
+		public String indexCommand(String owningType, String[] properties) {
+			String name = owningType.toLowerCase();
+
+			StringBuilder sb = new StringBuilder();
+			sb.append("CONSTRAINT ON (`")
+					.append(name)
+					.append("`:`")
+					.append(owningType)
+					.append("`) ASSERT (");
+			appendPropertiesWithNode(sb, name, properties);
+			sb.append(") IS NODE KEY");
+			return sb.toString();
+		}
+	},
+
+	/**
+	 * Node property existence constraint
+	 */
+	NODE_PROP_EXISTENCE_CONSTRAINT {
+		@Override
+		public String indexCommand(String owningType, String[] properties) {
+			String name = owningType.toLowerCase();
+
+			if (properties.length != 1) {
+				throw new IllegalArgumentException(
+						NODE_PROP_EXISTENCE_CONSTRAINT + " must have exactly one property, got " +
+								Arrays.toString(properties));
+			}
+			return "CONSTRAINT ON (`" + name + "`:`" + owningType + "`) ASSERT exists(`" + name + "`.`"
+					+ properties[0] + "`)";
+		}
+	},
+
+	/**
+	 * Relationship property existence constraint
+	 */
+	REL_PROP_EXISTENCE_CONSTRAINT {
+		@Override
+		public String indexCommand(String owningType, String[] properties) {
+			String name = owningType.toLowerCase();
+
+			if (properties.length != 1) {
+				throw new IllegalArgumentException(
+						NODE_PROP_EXISTENCE_CONSTRAINT + " must have exactly one property, got " +
+								Arrays.toString(properties));
+			}
+			return "CONSTRAINT ON ()-[`" + name + "`:`" + owningType + "`]-() ASSERT exists(`" + name + "`.`"
+					+ properties[0] + "`)";
+		}
+	};
+
+	public String convertToCreateCommand(String owningType, String[] properties) {
+		return "CREATE " + indexCommand(owningType, properties);
+	}
+
+	protected abstract String indexCommand(String owningType, String[] properties);
+
+	private static void appendProperties(StringBuilder sb, String[] properties) {
+		for (int i = 0; i < properties.length; i++) {
+			sb.append('`');
+			sb.append(properties[i]);
+			sb.append('`');
+			if (i < (properties.length - 1)) {
+				sb.append(',');
+			}
+		}
+	}
+
+	private static void appendPropertiesWithNode(StringBuilder sb, String nodeName, String[] properties) {
+		for (int i = 0; i < properties.length; i++) {
+			sb.append('`');
+			sb.append(nodeName);
+			sb.append("`.`");
+			sb.append(properties[i]);
+			sb.append('`');
+			if (i < (properties.length - 1)) {
+				sb.append(',');
+			}
+		}
+	}
+
+}
---
+++ src/test/java/io/knowledgeassets/myskills/server/neo4j/IndexManagerTest.java
@@ -0,0 +1,77 @@
+package io.knowledgeassets.myskills.server.neo4j;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.neo4j.ogm.session.SessionFactory;
+
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+@ExtendWith(MockitoExtension.class)
+public class IndexManagerTest {
+
+	@Mock
+	private SessionFactory sessionFactory;
+
+	private IndexManager indexManager;
+
+	@BeforeEach
+	public void setUp() throws IOException {
+		indexManager = new IndexManager(sessionFactory, "src/main/resources/migration/generated_indexes.cql");
+	}
+
+	@Test
+	public void testThrowNoSuchFileException() {
+		assertThrows(NoSuchFileException.class,
+				() -> new IndexManager(sessionFactory, "wrong/path/to/file/wrong_filename.cql"));
+	}
+
+	@Test
+	public void parseWrongIndexTest() {
+		assertThat(indexManager.parseIndexToCreateStatement("some wrong string")).isNull();
+	}
+
+	@Test
+	public void parseCompositeIndexTest() {
+		assertThat(indexManager.parseIndexToCreateStatement("INDEX ON :Person(age, country)"))
+				.isEqualTo("CREATE INDEX ON :`Person`(`age`,`country`)");
+	}
+
+	@Test
+	public void parseSingleIndexTest() {
+		assertThat(indexManager.parseIndexToCreateStatement("INDEX ON :Person(firstname)"))
+				.isEqualTo("CREATE INDEX ON :`Person`(`firstname`)");
+	}
+
+	@Test
+	public void parseUniqueConstraintTest() {
+		assertThat(indexManager.parseIndexToCreateStatement("CONSTRAINT ON (book:Book) ASSERT book.isbn IS UNIQUE"))
+				.isEqualTo("CREATE CONSTRAINT ON (`book`:`Book`) ASSERT `book`.`isbn` IS UNIQUE");
+	}
+
+	@Test
+	public void parseNodeKeyConstraintTest() {
+		assertThat(indexManager.parseIndexToCreateStatement("CONSTRAINT ON (person:Person) ASSERT (person.firstname, person.surname) IS NODE KEY"))
+				.isEqualTo("CREATE CONSTRAINT ON (`person`:`Person`) ASSERT (`person`.`firstname`,`person`.`surname`) IS NODE KEY");
+	}
+
+	@Test
+	public void parseNodePropertyExistenceConstraintTest() {
+		assertThat(indexManager.parseIndexToCreateStatement("CONSTRAINT ON (book:Book) ASSERT exists(book.isbn)"))
+				.isEqualTo("CREATE CONSTRAINT ON (`book`:`Book`) ASSERT exists(`book`.`isbn`)");
+	}
+
+	@Test
+	public void parseRelationshipPropertyExistenceConstraintTest() {
+		assertThat(indexManager.parseIndexToCreateStatement("CONSTRAINT ON ()-[liked:LIKED]-() ASSERT exists(liked.day)"))
+				.isEqualTo("CREATE CONSTRAINT ON ()-[`liked`:`LIKED`]-() ASSERT exists(`liked`.`day`)");
+	}
+
+}
