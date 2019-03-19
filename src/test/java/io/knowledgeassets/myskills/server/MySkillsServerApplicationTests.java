cafa25a4737875f252ee57535b8b4ee7867a4a4e
Dmitry Lebedko <dmitry.lebedko@t-systems.com>
2019-03-19 13:12:37 +0100

    liquigraph integrated to make incremental updates on the database.

--- src/test/java/io/knowledgeassets/myskills/server/MySkillsServerApplicationTests.java
+++ src/test/java/io/knowledgeassets/myskills/server/MySkillsServerApplicationTests.java
@@ -1,28 +1,17 @@
 package io.knowledgeassets.myskills.server;
 
-import io.knowledgeassets.myskills.server.neo4j.IndexManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-
-import static org.mockito.Mockito.verify;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class MySkillsServerApplicationTests {
-
-	@MockBean
-	private IndexManager mockIndexManager;
 
 	@Test
 	@DisplayName("Application context loads successfully")
 	void contextLoads() {
 	}
 
-	@Test
-	public void relatedProceduresAreRunning() {
-		verify(mockIndexManager).createIndexes();
-	}
 }
