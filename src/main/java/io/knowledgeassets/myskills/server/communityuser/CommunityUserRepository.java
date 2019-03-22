7bfa5891df1fde30950b3ab7890c9ef0d622d413
Dmitry Lebedko <dmitry.lebedko@t-systems.com>
2019-03-22 14:32:30 +0100

    The endpoint to get user suggestions for a community filtered by search parameter. Tests.

--- src/main/java/io/knowledgeassets/myskills/server/communityuser/CommunityUserRepository.java
+++ src/main/java/io/knowledgeassets/myskills/server/communityuser/CommunityUserRepository.java
@@ -1,7 +1,10 @@
 package io.knowledgeassets.myskills.server.communityuser;
 
 import io.knowledgeassets.myskills.server.community.CommunityRole;
+import io.knowledgeassets.myskills.server.user.User;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -17,5 +20,12 @@ public interface CommunityUserRepository extends Neo4jRepository<CommunityUser, 
 	Stream<CommunityUser> findByCommunityId(String communityId);
 
 	Stream<CommunityUser> findByCommunityIdAndRole(String communityId, CommunityRole role);
+
+	@Query("MATCH (u:User) WHERE NOT (u)-[:COMMUNITY_USER]-(:Community {id: {communityId}}) " +
+			"AND ((toLower(u.userName) CONTAINS toLower({search}) " +
+			" OR toLower(u.firstName) CONTAINS toLower({search}) " +
+			" OR toLower(u.lastName) CONTAINS toLower({search})) " +
+			" OR {search} IS NULL) RETURN u")
+	Iterable<User> getUsersNotRelatedToCommunity(@Param("communityId") String communityId, @Param("search") String search);
 
 }
