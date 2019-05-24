5e08f4012ccd1ed54b70aa875fa258e0dc082f8c
Dmitry Lebedko <dmitry.lebedko@t-systems.com>
2019-05-24 11:33:36 +0200

    API GET /users/{userId}/projects is available to user's manager. Tests.

--- src/main/java/com/tsmms/skoop/userproject/query/UserProjectQueryController.java
+++ src/main/java/com/tsmms/skoop/userproject/query/UserProjectQueryController.java
@@ -1,7 +1,12 @@
 package com.tsmms.skoop.userproject.query;
 
 import com.tsmms.skoop.exception.NoSuchResourceException;
+import com.tsmms.skoop.exception.UserNotAuthorizedException;
 import com.tsmms.skoop.exception.enums.Model;
+import com.tsmms.skoop.security.CurrentUserService;
+import com.tsmms.skoop.user.User;
+import com.tsmms.skoop.user.query.UserPermissionQueryService;
+import com.tsmms.skoop.user.query.UserQueryService;
 import com.tsmms.skoop.userproject.UserProjectResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -18,14 +23,27 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.Objects.requireNonNull;
+import static java.lang.String.format;
+import static com.tsmms.skoop.user.UserPermissionScope.READ_USER_SKILLS;
+
 @Api(tags = "UserProjects")
 @RestController
 public class UserProjectQueryController {
 
 	private final UserProjectQueryService userProjectQueryService;
+	private final CurrentUserService currentUserService;
+	private final UserQueryService userQueryService;
+	private final UserPermissionQueryService userPermissionQueryService;
 
-	public UserProjectQueryController(UserProjectQueryService userProjectQueryService) {
-		this.userProjectQueryService = userProjectQueryService;
+	public UserProjectQueryController(UserProjectQueryService userProjectQueryService,
+									  CurrentUserService currentUserService,
+									  UserQueryService userQueryService,
+									  UserPermissionQueryService userPermissionQueryService) {
+		this.userProjectQueryService = requireNonNull(userProjectQueryService);
+		this.currentUserService = requireNonNull(currentUserService);
+		this.userQueryService = requireNonNull(userQueryService);
+		this.userPermissionQueryService = requireNonNull(userPermissionQueryService);
 	}
 
 	@ApiOperation(value = "Get all projects related to a specific user",
@@ -37,13 +55,27 @@ public class UserProjectQueryController {
 			@ApiResponse(code = 404, message = "Resource not found, e.g. user does not exist or skill not assigned"),
 			@ApiResponse(code = 500, message = "Error during execution")
 	})
-	@PreAuthorize("isPrincipalUserId(#userId) or hasUserPermission(#userId, 'READ_USER_SKILLS')")
+	@PreAuthorize("isAuthenticated()")
 	@GetMapping(path = "/users/{userId}/projects", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<UserProjectResponse>> getUserProjects(@PathVariable("userId") String userId) {
-		final List<UserProjectResponse> userProjects = userProjectQueryService.getUserProjects(userId)
-				.map(UserProjectResponse::of)
-				.collect(Collectors.toList());
-		return ResponseEntity.status(HttpStatus.OK).body(userProjects);
+		final User user = userQueryService.getUserById(userId)
+				.orElseThrow(() -> {
+					String[] searchParamsMap = {"id", userId};
+					return NoSuchResourceException.builder()
+							.model(Model.USER)
+							.searchParamsMap(searchParamsMap)
+							.build();
+				});
+		if (currentUserService.isAuthenticatedUserId(userId) ||
+				userPermissionQueryService.hasUserPermission(userId, currentUserService.getCurrentUserId(), READ_USER_SKILLS) ||
+				(user.getManager() != null && currentUserService.getCurrentUserId().equals(user.getManager().getId()))) {
+			final List<UserProjectResponse> userProjects = userProjectQueryService.getUserProjects(userId)
+					.map(UserProjectResponse::of)
+					.collect(Collectors.toList());
+			return ResponseEntity.status(HttpStatus.OK).body(userProjects);
+		} else {
+			throw new UserNotAuthorizedException(format("The user is not authorized to view projects of the user with ID %s", userId));
+		}
 	}
 
 	@ApiOperation(value = "Get specific project related to a specific user",
--- src/test/java/com/tsmms/skoop/userproject/query/UserProjectQueryControllerTests.java
+++ src/test/java/com/tsmms/skoop/userproject/query/UserProjectQueryControllerTests.java
@@ -4,6 +4,8 @@ import com.tsmms.skoop.project.Project;
 import com.tsmms.skoop.user.User;
 import com.tsmms.skoop.user.UserPermissionScope;
 import com.tsmms.skoop.common.AbstractControllerTests;
+import com.tsmms.skoop.user.query.UserPermissionQueryService;
+import com.tsmms.skoop.user.query.UserQueryService;
 import com.tsmms.skoop.userproject.UserProject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,6 +36,9 @@ class UserProjectQueryControllerTests extends AbstractControllerTests {
 
 	@MockBean
 	private UserProjectQueryService userProjectQueryService;
+
+	@MockBean
+	private UserQueryService userQueryService;
 
 	@Test
 	@DisplayName("Tests if user projects can be retrieved.")
@@ -99,6 +104,11 @@ class UserProjectQueryControllerTests extends AbstractControllerTests {
 				.id("1f37fb2a-b4d0-4119-9113-4677beb20ae2")
 				.userName("tester")
 				.build();
+
+		given(userQueryService.getUserById("1f37fb2a-b4d0-4119-9113-4677beb20ae2")).willReturn(
+				Optional.of(owner)
+		);
+
 		mockMvc.perform(get("/users/1f37fb2a-b4d0-4119-9113-4677beb20ae2/projects")
 				.with(authentication(withUser(owner))))
 				.andExpect(status().isOk())
@@ -150,6 +160,16 @@ class UserProjectQueryControllerTests extends AbstractControllerTests {
 				.id("2a29ca1o-b4d0-4119-9113-4677beb20ae2")
 				.userName("tester")
 				.build();
+		given(userQueryService.getUserById("1f37fb2a-b4d0-4119-9113-4677beb20ae2")).willReturn(
+				Optional.of(User.builder()
+						.id("1f37fb2a-b4d0-4119-9113-4677beb20ae2")
+						.userName("tester")
+						.firstName("test")
+						.lastName("testing")
+						.email("test@mail.com")
+						.build()
+				)
+		);
 		mockMvc.perform(get("/users/1f37fb2a-b4d0-4119-9113-4677beb20ae2/projects")
 				.with(authentication(withUser(owner))))
 				.andExpect(status().isForbidden());
@@ -230,6 +250,17 @@ class UserProjectQueryControllerTests extends AbstractControllerTests {
 		givenUser("1f37fb2a-b4d0-4119-9113-4677beb20ae2")
 				.hasAuthorizedUsers("2a29ca1o-b4d0-4119-9113-4677beb20ae2")
 				.forScopes(UserPermissionScope.READ_USER_SKILLS);
+
+		given(userQueryService.getUserById("1f37fb2a-b4d0-4119-9113-4677beb20ae2")).willReturn(
+				Optional.of(User.builder()
+						.id("1f37fb2a-b4d0-4119-9113-4677beb20ae2")
+						.userName("tester")
+						.firstName("test")
+						.lastName("testing")
+						.email("test@mail.com")
+						.build()
+				)
+		);
 
 		mockMvc.perform(get("/users/1f37fb2a-b4d0-4119-9113-4677beb20ae2/projects")
 				.with(authentication(withUser(owner))))
@@ -367,6 +398,127 @@ class UserProjectQueryControllerTests extends AbstractControllerTests {
 		mockMvc.perform(get("/users/1f37fb2a-b4d0-4119-9113-4677beb20ae2/projects/123")
 				.with(authentication(withUser(owner))))
 				.andExpect(status().isNotFound());
+	}
+
+	@Test
+	@DisplayName("Tests if user projects can be retrieved by user manager.")
+	void testIfUserProjectsCanBeRetrievedByUserManager() throws Exception {
+		given(userProjectQueryService.getUserProjects("1f37fb2a-b4d0-4119-9113-4677beb20ae2"))
+				.willReturn(Stream.of(
+						UserProject.builder()
+								.role("developer")
+								.tasks("development")
+								.startDate(LocalDate.of(2019, 1, 9))
+								.endDate(LocalDate.of(2019, 5, 1))
+								.id("aaa")
+								.creationDate(LocalDateTime.of(2019, 11, 3, 13, 30))
+								.lastModifiedDate(LocalDateTime.of(2019, 11, 3, 13, 31))
+								.user(User.builder()
+										.id("1f37fb2a-b4d0-4119-9113-4677beb20ae2")
+										.userName("tester")
+										.firstName("test")
+										.lastName("testing")
+										.email("test@mail.com")
+										.build()
+								)
+								.project(Project.builder()
+										.id("123")
+										.name("Simple project")
+										.industrySector("Software development")
+										.customer("Company")
+										.creationDate(LocalDateTime.of(2019, 1, 11, 11, 30))
+										.lastModifiedDate(LocalDateTime.of(2019, 1, 12, 10, 10))
+										.description("Some project description")
+										.build()
+								)
+								.build(),
+						UserProject.builder()
+								.role("designer")
+								.tasks("design")
+								.startDate(LocalDate.of(2018, 1, 9))
+								.endDate(LocalDate.of(2018, 5, 1))
+								.id("bbb")
+								.creationDate(LocalDateTime.of(2018, 11, 3, 13, 30))
+								.lastModifiedDate(LocalDateTime.of(2018, 11, 3, 13, 31))
+								.user(User.builder()
+										.id("1f37fb2a-b4d0-4119-9113-4677beb20ae2")
+										.userName("tester")
+										.firstName("test")
+										.lastName("testing")
+										.email("test@mail.com")
+										.build()
+								)
+								.project(Project.builder()
+										.id("456")
+										.name("Another project")
+										.industrySector("Software development")
+										.customer("Another company")
+										.creationDate(LocalDateTime.of(2018, 1, 11, 11, 30))
+										.lastModifiedDate(LocalDateTime.of(2018, 1, 12, 10, 10))
+										.description("Some project description")
+										.build()
+								)
+								.build()
+				));
+		final User manager = User.builder()
+				.id("2a29ca1o-b4d0-4119-9113-4677beb20ae2")
+				.userName("tester")
+				.build();
+
+		given(userQueryService.getUserById("1f37fb2a-b4d0-4119-9113-4677beb20ae2")).willReturn(
+				Optional.of(User.builder()
+						.id("1f37fb2a-b4d0-4119-9113-4677beb20ae2")
+						.userName("tester")
+						.firstName("test")
+						.lastName("testing")
+						.email("test@mail.com")
+						.manager(manager)
+						.build()
+				)
+		);
+
+		mockMvc.perform(get("/users/1f37fb2a-b4d0-4119-9113-4677beb20ae2/projects")
+				.with(authentication(withUser(manager))))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.length()", is(equalTo(2))))
+				.andExpect(jsonPath("$[0].id", is(equalTo("aaa"))))
+				.andExpect(jsonPath("$[0].role", is(equalTo("developer"))))
+				.andExpect(jsonPath("$[0].tasks", is(equalTo("development"))))
+				.andExpect(jsonPath("$[0].startDate", is(equalTo("2019-01-09"))))
+				.andExpect(jsonPath("$[0].endDate", is(equalTo("2019-05-01"))))
+				.andExpect(jsonPath("$[0].creationDate", is(equalTo("2019-11-03T13:30:00"))))
+				.andExpect(jsonPath("$[0].lastModifiedDate", is(equalTo("2019-11-03T13:31:00"))))
+				.andExpect(jsonPath("$[0].user.id", is(equalTo("1f37fb2a-b4d0-4119-9113-4677beb20ae2"))))
+				.andExpect(jsonPath("$[0].user.userName", is(equalTo("tester"))))
+				.andExpect(jsonPath("$[0].user.firstName", is(equalTo("test"))))
+				.andExpect(jsonPath("$[0].user.lastName", is(equalTo("testing"))))
+				.andExpect(jsonPath("$[0].user.email", is(equalTo("test@mail.com"))))
+				.andExpect(jsonPath("$[0].project.id", is(equalTo("123"))))
+				.andExpect(jsonPath("$[0].project.name", is(equalTo("Simple project"))))
+				.andExpect(jsonPath("$[0].project.industrySector", is(equalTo("Software development"))))
+				.andExpect(jsonPath("$[0].project.customer", is(equalTo("Company"))))
+				.andExpect(jsonPath("$[0].project.description", is(equalTo("Some project description"))))
+				.andExpect(jsonPath("$[0].project.creationDate", is(equalTo("2019-01-11T11:30:00"))))
+				.andExpect(jsonPath("$[0].project.lastModifiedDate", is(equalTo("2019-01-12T10:10:00"))))
+				.andExpect(jsonPath("$[1].id", is(equalTo("bbb"))))
+				.andExpect(jsonPath("$[1].role", is(equalTo("designer"))))
+				.andExpect(jsonPath("$[1].tasks", is(equalTo("design"))))
+				.andExpect(jsonPath("$[1].startDate", is(equalTo("2018-01-09"))))
+				.andExpect(jsonPath("$[1].endDate", is(equalTo("2018-05-01"))))
+				.andExpect(jsonPath("$[1].creationDate", is(equalTo("2018-11-03T13:30:00"))))
+				.andExpect(jsonPath("$[1].lastModifiedDate", is(equalTo("2018-11-03T13:31:00"))))
+				.andExpect(jsonPath("$[1].user.id", is(equalTo("1f37fb2a-b4d0-4119-9113-4677beb20ae2"))))
+				.andExpect(jsonPath("$[1].user.userName", is(equalTo("tester"))))
+				.andExpect(jsonPath("$[1].user.firstName", is(equalTo("test"))))
+				.andExpect(jsonPath("$[1].user.lastName", is(equalTo("testing"))))
+				.andExpect(jsonPath("$[1].user.email", is(equalTo("test@mail.com"))))
+				.andExpect(jsonPath("$[1].project.id", is(equalTo("456"))))
+				.andExpect(jsonPath("$[1].project.name", is(equalTo("Another project"))))
+				.andExpect(jsonPath("$[1].project.industrySector", is(equalTo("Software development"))))
+				.andExpect(jsonPath("$[1].project.customer", is(equalTo("Another company"))))
+				.andExpect(jsonPath("$[1].project.description", is(equalTo("Some project description"))))
+				.andExpect(jsonPath("$[1].project.creationDate", is(equalTo("2018-01-11T11:30:00"))))
+				.andExpect(jsonPath("$[1].project.lastModifiedDate", is(equalTo("2018-01-12T10:10:00"))));
 	}
 
 }
